06564e626ec21947564e914f3d0ffcfd23bb415a
rltran-codex <rtran9@montgomerycollege.edu>
2024-01-04 18:58:07 -0500

    simplified hashing using reduce

--- src/day15.py
+++ src/day15.py
@@ -1,21 +1,14 @@
+from functools import reduce
 
-INPUT_FILE = './resources/day15_input.txt'
-# INPUT_FILE = './resources/sample_input/day15.txt'
+# INPUT_FILE = './resources/day15_input.txt'
+INPUT_FILE = './resources/sample_input/day15.txt'
 
 with open(INPUT_FILE, 'r') as file:
   data = file.read().split(',')
 
 
 def hash_str(line : str):
-  line = [c for c in line]
-  current_val = 0
-
-  for c in line:
-    current_val += ord(c)
-    current_val *= 17
-    current_val = current_val % 256
-  
-  return current_val
+  return reduce(lambda current_val, c: (current_val + ord(c)) * 17 % 256, line, 0)
 
 hash_sum = 0
 for d in data:
