47b371257e306205aa8fb058763f26faf2cd7e1b
rltran-codex <rtran9@montgomerycollege.edu>
2024-01-20 23:55:33 -0500

    refactored to use interface

--- src/day16.ts
+++ src/day16.ts
@@ -4,11 +4,17 @@ const lines = fs.readFileSync("./resources/day16_input.txt", "utf-8");
 // const lines = fs.readFileSync("./resources/sample_input/day16.txt", "utf-8");
 const data: string[][] = lines.split("\r\n").map((line) => Array.from(line));
 
-enum beam_direction {
+enum BeamDirection {
   UP,
   DOWN,
   RIGHT,
   LEFT,
+}
+
+interface BeamCoordinate {
+  row:number;
+  col:number;
+  dir:BeamDirection;
 }
 
 function energizePlatform(data: string[][], stepStack : any[]) {
@@ -20,11 +26,8 @@ function energizePlatform(data: string[][], stepStack : any[]) {
   );
   // deploy beam and evaluate the number of energized tiles
   while (stepStack.length != 0) {
-    let curr_loc = stepStack.pop();
-    let curr_dir = curr_loc.shift();
-    let row = curr_loc[0][0];
-    let col = curr_loc[0][1];
-    curr_loc = curr_loc[0];
+    let { dir, row, col } = stepStack.pop() as BeamCoordinate;
+    let curr_loc = [row, col];
 
     // validate the row and col
     if (row < 0 || row > rows - 1 || col < 0 || col > cols - 1) {
@@ -33,15 +36,16 @@ function energizePlatform(data: string[][], stepStack : any[]) {
 
     const eval_pos = data[row][col];
     if (eval_pos === ".") {
-      stepStack.push([curr_dir, direction_handler(curr_dir, curr_loc)]);
+      let [row, col] = direction_handler(dir, curr_loc);
+      stepStack.push({dir:dir, row:row, col:col});
     } else if (eval_pos === "\\") {
-      handleBackSlash(curr_dir, stepStack, curr_loc);
+      handleBackSlash(dir, stepStack, curr_loc);
     } else if (eval_pos === "/") {
-      handleForwardSlash(curr_dir, stepStack, curr_loc);
+      handleForwardSlash(dir, stepStack, curr_loc);
     } else if (eval_pos === "|" && e_platform[row][col] !== "#") {
-      handleVerticalSplit(curr_dir, stepStack, curr_loc);
+      handleVerticalSplit(dir, stepStack, curr_loc);
     } else if (eval_pos === "-" && e_platform[row][col] !== "#") {
-      handleHorizontalSplit(curr_dir, stepStack, curr_loc);
+      handleHorizontalSplit(dir, stepStack, curr_loc);
     }
 
     e_platform[row][col] = "#";
@@ -53,74 +57,92 @@ function energizePlatform(data: string[][], stepStack : any[]) {
 }
 
 function handleVerticalSplit(
-  dir: beam_direction,
+  dir: BeamDirection,
   stack: any[],
   coordinates: number[]
 ) {
   if (
-    dir === (beam_direction.DOWN as beam_direction) ||
-    dir === (beam_direction.UP as beam_direction)
+    dir === (BeamDirection.DOWN as BeamDirection) ||
+    dir === (BeamDirection.UP as BeamDirection)
   ) {
-    stack.push([dir, direction_handler(dir, coordinates)]);
+    const [row, col] = direction_handler(dir, coordinates);
+    stack.push({dir:dir, row:row, col:col});
     return;
   }
 
+  let [row, col] = direction_handler(BeamDirection.DOWN, coordinates);
   // add the left
   stack.push([
-    beam_direction.DOWN,
-    direction_handler(beam_direction.DOWN, coordinates),
+    {
+      dir:BeamDirection.DOWN,
+      row:row,
+      col:col
+    }
   ]);
   // add the right
+  [row, col] = direction_handler(BeamDirection.UP, coordinates);
   stack.push([
-    beam_direction.UP,
-    direction_handler(beam_direction.UP, coordinates),
+    {
+      dir:BeamDirection.UP,
+      row:row,
+      col:col
+    }
   ]);
 }
 
 function handleHorizontalSplit(
-  dir: beam_direction,
+  dir: BeamDirection,
   stack: any[],
   coordinates: number[]
 ) {
   if (
-    dir === (beam_direction.RIGHT as beam_direction) ||
-    dir === (beam_direction.LEFT as beam_direction)
+    dir === (BeamDirection.LEFT as BeamDirection) ||
+    dir === (BeamDirection.RIGHT as BeamDirection)
   ) {
-    stack.push([dir, direction_handler(dir, coordinates)]);
+    const [row, col] = direction_handler(dir, coordinates);
+    stack.push({dir:dir, row:row, col:col});
     return;
   }
 
+  let [row, col] = direction_handler(BeamDirection.RIGHT, coordinates);
   // add the left
   stack.push([
-    beam_direction.LEFT,
-    direction_handler(beam_direction.LEFT, coordinates),
+    {
+      dir:BeamDirection.RIGHT,
+      row:row,
+      col:col
+    }
   ]);
   // add the right
+  [row, col] = direction_handler(BeamDirection.LEFT, coordinates);
   stack.push([
-    beam_direction.RIGHT,
-    direction_handler(beam_direction.RIGHT, coordinates),
+    {
+      dir:BeamDirection.LEFT,
+      row:row,
+      col:col
+    }
   ]);
 }
 
 function handleBackSlash(
-  dir: beam_direction,
+  dir: BeamDirection,
   stack: any[],
   coordinates: number[]
 ) {
   switch (
     dir // "\"
   ) {
-    case beam_direction.UP as beam_direction:
-      stack.push([beam_direction.LEFT, [coordinates[0], coordinates[1] - 1]]);
+    case BeamDirection.UP as BeamDirection:
+      stack.push({dir: BeamDirection.LEFT, row:coordinates[0], col: coordinates[1] - 1});
       break;
-    case beam_direction.DOWN as beam_direction:
-      stack.push([beam_direction.RIGHT, [coordinates[0], coordinates[1] + 1]]);
+    case BeamDirection.DOWN as BeamDirection:
+      stack.push({dir: BeamDirection.RIGHT, row:coordinates[0], col: coordinates[1] + 1});
       break;
-    case beam_direction.RIGHT as beam_direction:
-      stack.push([beam_direction.DOWN, [coordinates[0] + 1, coordinates[1]]]);
+    case BeamDirection.RIGHT as BeamDirection:
+      stack.push({dir: BeamDirection.DOWN, row:coordinates[0] + 1, col: coordinates[1]});
       break;
-    case beam_direction.LEFT as beam_direction:
-      stack.push([beam_direction.UP, [coordinates[0] - 1, coordinates[1]]]);
+    case BeamDirection.LEFT as BeamDirection:
+      stack.push({dir: BeamDirection.UP, row:coordinates[0] - 1, col: coordinates[1]});
       break;
 
     default:
@@ -129,24 +151,24 @@ function handleBackSlash(
 }
 
 function handleForwardSlash(
-  dir: beam_direction,
+  dir: BeamDirection,
   stack: any[],
   coordinates: number[]
 ) {
   switch (
     dir // "/"
   ) {
-    case beam_direction.UP as beam_direction:
-      stack.push([beam_direction.RIGHT, [coordinates[0], coordinates[1] + 1]]);
+    case BeamDirection.UP as BeamDirection:
+      stack.push({dir: BeamDirection.RIGHT, row: coordinates[0], col: coordinates[1] + 1});
       break;
-    case beam_direction.DOWN as beam_direction:
-      stack.push([beam_direction.LEFT, [coordinates[0], coordinates[1] - 1]]);
+    case BeamDirection.DOWN as BeamDirection:
+      stack.push({dir: BeamDirection.LEFT, row: coordinates[0], col: coordinates[1] - 1});
       break;
-    case beam_direction.RIGHT as beam_direction:
-      stack.push([beam_direction.UP, [coordinates[0] - 1, coordinates[1]]]);
+    case BeamDirection.RIGHT as BeamDirection:
+      stack.push({dir: BeamDirection.UP, row: coordinates[0] - 1, col: coordinates[1]});
       break;
-    case beam_direction.LEFT as beam_direction:
-      stack.push([beam_direction.DOWN, [coordinates[0] + 1, coordinates[1]]]);
+    case BeamDirection.LEFT as BeamDirection:
+      stack.push({dir: BeamDirection.DOWN, row: coordinates[0] + 1, col: coordinates[1]});
       break;
 
     default:
@@ -154,24 +176,24 @@ function handleForwardSlash(
   }
 }
 
-function direction_handler(direction: beam_direction, coordinates: number[]) {
+function direction_handler(direction: BeamDirection, coordinates: number[]) {
   let row: number;
   let col: number;
 
   switch (direction) {
-    case beam_direction.UP:
+    case BeamDirection.UP:
       row = coordinates[0] - 1;
       col = coordinates[1];
       break;
-    case beam_direction.DOWN:
+    case BeamDirection.DOWN:
       row = coordinates[0] + 1;
       col = coordinates[1];
       break;
-    case beam_direction.RIGHT:
+    case BeamDirection.RIGHT:
       row = coordinates[0];
       col = coordinates[1] + 1;
       break;
-    case beam_direction.LEFT:
+    case BeamDirection.LEFT:
       row = coordinates[0];
       col = coordinates[1] - 1;
       break;
@@ -187,31 +209,31 @@ function findMaxEnergy(data : string[][]) {
   const cols = data[0].length;
 
   // upper left
-  edge_points.push([beam_direction.RIGHT, [0,0]]);
-  edge_points.push([beam_direction.DOWN, [0, 0]]);
-
+  edge_points.push({dir:BeamDirection.RIGHT, row:0, col:0});
+  edge_points.push({dir: BeamDirection.DOWN, row:0, col:0});
+  
   // upper right
-  edge_points.push([beam_direction.LEFT, [0, cols - 1]]);
-  edge_points.push([beam_direction.DOWN, [0, cols - 1]]);
+  edge_points.push({dir: BeamDirection.LEFT, row:0, col:cols - 1});
+  edge_points.push({dir: BeamDirection.DOWN, row:0, col:cols - 1});
 
   // lower left
-  edge_points.push([beam_direction.UP, [rows - 1, 0]]);
-  edge_points.push([beam_direction.RIGHT, [rows - 1, 0]]);
+  edge_points.push({dir: BeamDirection.UP, row:rows - 1, col:0});
+  edge_points.push({dir: BeamDirection.RIGHT, row:rows - 1, col:0});
 
   // lower right
-  edge_points.push([beam_direction.UP, [rows - 1, cols - 1]]);
-  edge_points.push([beam_direction.LEFT, [rows - 1, cols - 1]]);
+  edge_points.push({dir: BeamDirection.UP, row:rows - 1, col: cols - 1});
+  edge_points.push({dir: BeamDirection.LEFT, row:rows - 1, col: cols - 1});
 
   // upper and lower most edges
   for (let col = 1; col < cols - 1; col++) {
-    edge_points.push([beam_direction.DOWN, [0, col]]);
-    edge_points.push([beam_direction.UP, [rows - 1, col]]);
+    edge_points.push({dir: BeamDirection.DOWN, row:0, col: col});
+    edge_points.push({dir: BeamDirection.UP, row:rows - 1, col: col});
   }
 
   // right and left most edges
   for (let row = 1; row < rows - 1; row++) {
-    edge_points.push([beam_direction.RIGHT, [row, 0]]);
-    edge_points.push([beam_direction.LEFT, [row, cols - 1]]);
+    edge_points.push({dir: BeamDirection.RIGHT, row:row, col: 0});
+    edge_points.push({dir: BeamDirection.LEFT, row:row, col: cols - 1});
   }
 
   var max = Number.MIN_SAFE_INTEGER;
@@ -229,15 +251,16 @@ function findMaxEnergy(data : string[][]) {
 
 let start = performance.now();
 let stepStack = new Array();
-stepStack.push([beam_direction.RIGHT, [0, 0]]);
+stepStack.push({dir:BeamDirection.RIGHT, row:0, col:0});
 const result_1 = energizePlatform(data, stepStack);
 let end = performance.now();
 console.log(`Part 1, energized tiles: ${result_1}`);
 console.log(`Time: ${(end - start)} ms`);
 
-// for part two, its a little more time consuming, 
-// but lets start with the corners first
-// since those can be evaluated twice
+// for part two, its a little more time consuming
+// the idea is to mark all the starting points and their direction
+// and feed it to energizePlatform() method to calculate the number
+// of energized tiles
 
 start = performance.now();
 let result_2 = findMaxEnergy(data);
