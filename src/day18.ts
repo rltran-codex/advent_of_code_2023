400671db7790cab3e9c22b44fd6bb377fc20a9e0
rltran-codex <rtran9@montgomerycollege.edu>
2024-03-03 22:32:14 -0500

    day 18 - completed

--- src/day18.ts
+++ src/day18.ts
@@ -38,25 +38,80 @@ function dig(dInstrs: digInstruction[]): number {
   while (dInstrs.length > 0) {
     const { dir, n_steps, color } = dInstrs.shift();
     for (let n = 0; n < n_steps; n++) {
-      point = dirHandler(dir, point);
+      point = dirHandler(dir, point, 1);
       lpoints.push({ point: point, color: color });
     }
   }
 
   // using shoelace and  pick's theorem to find the area of the polygon
-  return calculateInteriorPoints(lpoints) + lpoints.length;
+  return calculateInteriorPoints(lpoints, lpoints.length) + lpoints.length;
 }
 
-function dirHandler(dir: string, coordinate: number[]) {
+
+function hexDig(encodedInstrs: digInstruction[]): number {
+  function decodeDir(dir:number) {
+    switch (dir) {
+      case 0:
+        return 'R';
+      case 1:
+        return 'D';
+      case 2:
+        return 'L';
+      case 3:
+        return 'U';
+    }
+  }
+  
+  // decode new dig instructions
+  const decodedInstrs = [];
+  encodedInstrs.forEach((v) => {
+    let [n_steps, dir] = hexExtract(v.color);
+    let n_dir = decodeDir(dir);
+    decodedInstrs.push({dir:n_dir, n_steps:n_steps});
+  });
+
+  let point = [0, 0];
+  let lpoints = [];
+  let total = 0;
+
+  // these are too large of values, so only use the beginning, middle,  and end of lattice points
+  while (decodedInstrs.length > 0) {
+    const {dir, n_steps} = decodedInstrs.shift();
+    // take only the beginning point, middle point, and the ending point; mimic creating a 'line' between the two points
+    const s_point = dirHandler(dir, point, 1);
+    const m_point = dirHandler(dir, point, Math.floor(n_steps / 2));
+    const e_point = dirHandler(dir, point, n_steps);
+
+    point = e_point; // update 'pencil point'
+
+    lpoints.push({point:s_point});
+    lpoints.push({point:m_point});
+    lpoints.push({point:e_point});
+
+    total += n_steps;
+  }
+
+
+  return calculateInteriorPoints(lpoints, total) + total;
+}
+
+function hexExtract(hex: string) {
+  const n_steps: string = hex.substring(0, 5);
+  const dir: string = hex.substring(5);
+
+  return [parseInt(n_steps, 16), parseInt(dir, 16)];
+}
+
+function dirHandler(dir: string, coordinate: number[], n_steps: number) {
   switch (dir) {
     case "U":
-      return [coordinate[0] - 1, coordinate[1]];
+      return [coordinate[0] - n_steps, coordinate[1]];
     case "D":
-      return [coordinate[0] + 1, coordinate[1]];
+      return [coordinate[0] + n_steps, coordinate[1]];
     case "L":
-      return [coordinate[0], coordinate[1] - 1];
+      return [coordinate[0], coordinate[1] - n_steps];
     case "R":
-      return [coordinate[0], coordinate[1] + 1];
+      return [coordinate[0], coordinate[1] + n_steps];
   }
 }
 
@@ -67,7 +122,7 @@ function dirHandler(dir: string, coordinate: number[]) {
  * @param points array of boundary lattice points
  * @returns 
  */
-function calculateInteriorPoints(points: digPoint[]) {
+function calculateInteriorPoints(points: digPoint[], boundaryPoints: number) {
   const numPoints = points.length;
   let area = 0;
 
@@ -80,7 +135,7 @@ function calculateInteriorPoints(points: digPoint[]) {
 
   area = Math.abs(area) / 2;
 
-  return area - (numPoints / 2) + 1;
+  return area - (boundaryPoints / 2) + 1;
 }
 
 
@@ -88,4 +143,9 @@ function calculateInteriorPoints(points: digPoint[]) {
 let start = performance.now();
 console.log(`part 1: ${dig(readFile("./resources/day18_input.txt"))}`);
 let end = performance.now();
+console.log(`Time: ${(end - start)} ms`);
+
+start = performance.now();
+console.log(`part 2: ${hexDig(readFile("./resources/day18_input.txt"))}`);
+end = performance.now();
 console.log(`Time: ${(end - start)} ms`);
